4f0a125c064f4cff5a0e121bf50bc0e9d3b302e8
slepyyz <szontaghagoston@gmail.com>
2026-03-18 14:44:18 +0100

    sakk utesek kesz

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { SAKKOZOK } from "./adatok.js";
-import { adatokMegjelenit, rendezesek } from "./fuggvenyek.js";
+import { adatokMegjelenit, utiE, rendezesek } from "./fuggvenyek.js";
+
+
 
 adatokMegjelenit(SAKKOZOK);
 rendezesek(SAKKOZOK);
+utiE();
